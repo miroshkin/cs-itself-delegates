4feb2328c007e399e55176a193318e9805da0d38
Anton Miroshkin <anton.miroshkin@gmail.com>
2019-08-09 10:05:04 +0300

    Added solution

---
+++ CS.Itself.Delegates/CS.Itself.Delegates/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CS.Itself.Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
